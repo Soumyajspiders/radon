213bf124815cec73fe788006ee0e5528a12ff7b6
Soumyajspiders <91654954+Soumyajspiders@users.noreply.github.com>
2022-06-09 14:20:39 +0530

    mongo-season-optional assignment

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,3 +1,4 @@
+const bookModel = require("../models/bookModel")
 const BookModel= require("../models/bookModel")
 
 const createBook= async function (req, res) {
@@ -7,10 +8,29 @@ const createBook= async function (req, res) {
     res.send({msg: savedData})
 }
 
-const getBooksData= async function (req, res) {
-    let allBooks= await BookModel.find(  { authorName : "SK" , isPublished: true }  )
-    res.send({msg: allBooks})
+const getbookbyAuthorId= async function (req, res) {
+    let authorId=req.params.requestId
+    let allBooks= await BookModel.find( {author_id:authorid} ).select({bookName:1,_id:0})
+    console.log(allBooks)
+    if(allBooks.length===0){
+        res.send({msg:"No books by this Author id"})
+    }else{
+        res.send({msg: allBooks})
+    }
+    
 }
 
+const getauthorByage= async function(req,res){
+    let getauthor=await authorModel.find({rating:{$gt:4}}).select({author_id:1,_id:0})
+    console.log(getauthor)
+    let authorInfo 
+          for (let i=0;i<getauthor.length;i++){
+              authorInfo=await authorModel.find({$and:[{age:{$gt:50}},getauthor[i]]}).select({authorName:1,_id:0})
+          }
+          res.send({msg:authorInfo})
+}
+
+module.exports.getbookbyAuthorId=getbookbyAuthorId
+module.exports.getauthorByage=getauthorByage
 module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
+// module.exports.getBooksData= getBooksData
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const router = express.Router();
 // const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
+const UserController= require("../controllers/authorController")
 const BookController= require("../controllers/bookController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+// router.post("/createUser", UserController.createUser  )
 
-router.get("/getUsersData", UserController.getUsersData)
+// router.get("/getUsersData", UserController.getUsersData)
 
-router.post("/createBook", BookController.createBook  )
+// router.post("/createBook", BookController.createBook  )
 
-router.get("/getBooksData", BookController.getBooksData)
+// router.get("/getBooksData", BookController.getBooksData)
+router.get("/getbookbyAuthorId/:requestId", BookController.getbookbyAuthorId)
+router.get("/getauthorByage", BookController.getauthorByage)
+
 
 module.exports = router;
