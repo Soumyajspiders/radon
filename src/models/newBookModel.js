6ad8edc2a9f5eb0237743d362e454dfc479d5d34
Soumyajspiders <91654954+Soumyajspiders@users.noreply.github.com>
2022-06-10 00:26:31 +0530

    populate-reference

---
+++ src/models/newBookModel.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const bookSchema = new mongoose.Schema( {
+    name: String,
+    author: {
+        type: ObjectId,
+        ref: "newAuthor"
+    },
+    price: Number,
+    ratings: Number,
+    publisher: {
+        type: ObjectId,
+        ref: "newPublisher"
+    },    
+    hardCover:{
+        type:Boolean,
+        default:false,
+    }
+
+}, { timestamps: true });
+
+
+module.exports = mongoose.model('newBook', bookSchema)
+
+
+
+// const mongoose = require('mongoose');
+// const ObjectId = mongoose.Schema.Types.ObjectId
+
+// const bookSchema = new mongoose.Schema( {
+//     name: String,
+//     author_id: {
+//         type: ObjectId,
+//         ref: "Author"
+//     },
+//     price: Number,
+//     ratings: Number
+
+
+// }, { timestamps: true });
+
+
+
+// module.exports = mongoose.model('LibraryBook', bookSchema)
