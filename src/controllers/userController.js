0b17afa5fd767c0194547687857c5793c546cfc1
Soumyajspiders <91654954+Soumyajspiders@users.noreply.github.com>
2022-06-14 23:18:06 +0530

    session/auth-1

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,14 +1,14 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
-const createUser = async function (abcd, xyz) {
+const createUser = async function (req, res) {
   //You can name the req, res objects anything.
   //but the first parameter is always the request 
   //the second parameter is always the response
-  let data = abcd.body;
+  let data = req.body;
   let savedData = await userModel.create(data);
-  console.log(abcd.newAtribute);
-  xyz.send({ msg: savedData });
+  // console.log(req.newAtribute);
+  res.send({ msg: savedData });
 };
 
 const loginUser = async function (req, res) {
@@ -19,7 +19,7 @@ const loginUser = async function (req, res) {
   if (!user)
     return res.send({
       status: false,
-      msg: "username or the password is not corerct",
+      msg: "Bhag Yaha Se",
     });
 
   // Once the login is successful, create the jwt token with sign function
@@ -31,7 +31,7 @@ const loginUser = async function (req, res) {
   let token = jwt.sign(
     {
       userId: user._id.toString(),
-      batch: "thorium",
+      batch: "radon",
       organisation: "FunctionUp",
     },
     "functionup-radon"
@@ -43,11 +43,12 @@ const loginUser = async function (req, res) {
 const getUserData = async function (req, res) {
   let token = req.headers["x-Auth-token"];
   if (!token) token = req.headers["x-auth-token"];
-
-  //If no token is present in the request header return error
   if (!token) return res.send({ status: false, msg: "token must be present" });
 
-  console.log(token);
+  //If no token is present in the request header return error
+  
+
+  // console.log(token);
   
   // If a token is present then decode the token with verify function
   // verify takes two inputs:
@@ -66,6 +67,28 @@ const getUserData = async function (req, res) {
   res.send({ status: true, data: userDetails });
 };
 
+
+const deletedUserData = async function (req, res) {
+  // Do the same steps here:
+  // Check if the token is present
+  // Check if the token present is a valid token
+  // Return a different error message in both these cases
+  
+    let userId = req.params.userId;
+    let user = await userModel.findById(userId);
+    let token = req.headers["x-Auth-token"];
+    if (!token) token = req.headers["x-auth-token"];
+    //Return an error if no user with the given id exists in the db
+    if (!user) {
+      return res.send("No such user exists");
+    }else if(!token) return res.send({ status: false, msg: "Token dikha nehi hei to Bhag" });
+  
+    let userData = req.body;
+    let updatedUser = await userModel.findOneAndUpdate({isDeleted:"true"});
+    res.send({ status: updatedUser, data: updatedUser });
+  };
+
+
 const updateUser = async function (req, res) {
 // Do the same steps here:
 // Check if the token is present
@@ -74,10 +97,12 @@ const updateUser = async function (req, res) {
 
   let userId = req.params.userId;
   let user = await userModel.findById(userId);
+  let token = req.headers["x-Auth-token"];
+  if (!token) token = req.headers["x-auth-token"];
   //Return an error if no user with the given id exists in the db
   if (!user) {
     return res.send("No such user exists");
-  }
+  }else if(!token) return res.send({ status: false, msg: "Token dikha nehi hei to Bhag" });
 
   let userData = req.body;
   let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
@@ -88,3 +113,4 @@ module.exports.createUser = createUser;
 module.exports.getUserData = getUserData;
 module.exports.updateUser = updateUser;
 module.exports.loginUser = loginUser;
+module.exports.deletedUserData = deletedUserData;
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://soumya123:M7Oyc7PZRPkKCvNY@cluster0.6ita8.mongodb.net/soumyaDb", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
